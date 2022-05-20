2be0614d97d30b6a8e9785bf9f7d53fe6c84de24
lorrainetechnology <lorrainetechnology@gmail.com>
2022-05-20 17:10:16 +0530

    unity container multiple implementations DI

---
+++ UnityContainerApp/ICar.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityContainerApp
+{
+    public interface ICar
+    {
+        int Drive();
+    }
+
+    public class BMW : ICar
+    {
+        private int _miles = 0;
+
+        public int Drive()
+        {
+            return ++_miles;
+        }
+    }
+
+    public class Ford: ICar
+    {
+        private int _miles = 0;
+
+        public int Drive()
+        {
+            return ++_miles;
+        }
+    }
+
+    public class Audi : ICar
+    {
+        private int _miles = 0;
+
+        public int Drive()
+        {
+            return ++_miles;
+        }
+    }
+
+    public class Driver
+    {
+        private ICar _car = null;
+        public Driver(ICar car)
+        {
+            _car = car;
+        }
+
+        public void DriveCar()
+        {
+            Console.WriteLine("Driving {0} at {1} miles per hour", _car.GetType().Name, _car.Drive());
+            Console.ReadLine();
+        }
+    }
+
+
+}
---
+++ UnityContainerApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Injection;
+
+namespace UnityContainerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer unityContainer = new UnityContainer();
+            unityContainer.RegisterType<ICar, BMW>();
+            unityContainer.RegisterType<ICar, Audi>("LuxuryCar");
+            unityContainer.RegisterType<Driver>("LuxuryCarDriver", new InjectionConstructor(unityContainer.Resolve<ICar>("LuxuryCar")));
+            
+            Driver driver1 = unityContainer.Resolve<Driver>();
+            driver1.DriveCar();
+
+            Driver driver2 = unityContainer.Resolve<Driver>("LuxuryCarDriver");
+            driver2.DriveCar();
+        }
+    }
+}
